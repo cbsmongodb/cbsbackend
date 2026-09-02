c38f9ba4946270d30eb60836fc077b0912322fe6
lizzaaabb <lbogveradze12@gmail.com>
2026-09-02 12:14:47 +0400

    populate parent on region routes

--- src/routes/admin/admin.routes.js
+++ src/routes/admin/admin.routes.js
@@ -26,7 +26,7 @@ function makeRouter(Model, populate = "") {
 export const roleRoutes = makeRouter(Role);
 export const designationRoutes = makeRouter(Designation);
 export const groupRoutes = makeRouter(Group, "region section head");
-export const regionRoutes = makeRouter(Region);
+export const regionRoutes = makeRouter(Region, "parent");
 
 export const sectionRoutes = makeRouter(Section, "region head");
 
